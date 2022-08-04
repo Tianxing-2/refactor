37c70719a27503101df7c008915a8b2d8e428501
Tianxing-2 <69039485+Tianxing-2@users.noreply.github.com>
2022-08-04 16:51:24 +0800

    Update RefactorApplicationTests.java

--- src/test/java/cn/eccom/refactor/RefactorApplicationTests.java
+++ src/test/java/cn/eccom/refactor/RefactorApplicationTests.java
@@ -1,17 +1,21 @@
 package cn.eccom.refactor;
 
+import cn.eccom.refactor.ch1.service.Prototype;
+import cn.eccom.refactor.ch1.util.R;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
+import javax.annotation.Resource;
+
 
 @SpringBootTest
 class RefactorApplicationTests {
-
+    @Resource
+    Prototype prototype;
     @Test
-    void contextLoads() {
+    public void test(){
+        R data = prototype.createStatementData();
+        System.out.println(prototype.statement(data));
+        System.out.println(prototype.htmlStatement(data));
     }
-
-    int a = 0; int b = 0; int c = 0;
-
 }
